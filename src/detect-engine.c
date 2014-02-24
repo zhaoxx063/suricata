cf72867756962d982b6d06bafdea6cad3e6460f9
Victor Julien <victor@inliniac.net>
2014-02-24 12:18:02 +0100

    Unreachable code handling: return_UNREACHABLE
    Wrapper for return statements that are unreachable according to
    clangs -Wunreachable-code. However, in some cases removing these
    will lead to GCC complaining "error: control reaches end of
    non-void function [-Werror=return-type]"
    This has been observed in functions only containing a switch
    statement, where all the cases of an enum were handled in the
    switch, and of them return directly.

--- src/detect-engine.c
+++ src/detect-engine.c
@@ -1577,6 +1577,7 @@ const char *DetectSigmatchListEnumToString(enum DetectSigmatchListEnum type) {
         case DETECT_SM_LIST_NOTSET:
             return "not set (internal)";
     }
+    return_UNREACHABLE("error");
 }
 
 
